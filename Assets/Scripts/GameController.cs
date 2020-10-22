561d30a8494207bc5e50cb8efa6a1729d9eec93c
Bruno de Paula <brunohpaula@gmail.com>
2020-10-22 13:56:37 +0100

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,29 +7,12 @@ public class GameController : MonoBehaviour
 
     public GameObject myObstaclePrefab;
 
-    float range = 3f;
-
-    int maxObstacles = 2000;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject whatever;        
-        /*
-        int counter = 0;
-
-        while(counter < maxObstacles)
-        {
-            whatever = Instantiate(myObstaclePrefab);
-            whatever.transform.position = new Vector3(Random.Range(-range, range), Random.Range(0, range), Random.Range(-range, range));
-            counter++;
-        }
-        */
-        for(int counter = 0; counter < maxObstacles; counter++)
-        {
-            whatever = Instantiate(myObstaclePrefab);
-            whatever.transform.position = new Vector3(Random.Range(-range, range), Random.Range(0, range), Random.Range(-range, range));
-        }
+        
     }
 
     // Update is called once per frame
